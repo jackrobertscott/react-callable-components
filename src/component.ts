2ed8e28dee71f46ab79ebbe713cd4ee7bf2b9111
Jack Scott <jack.rob.scott@gmail.com>
2023-12-16 14:01:43 +0800

    another attempted fix

--- src/component.ts
+++ src/component.ts
@@ -12,7 +12,7 @@ export type FCBetter<T> = (
  * @returns A function that takes optional props and returns a React element.
  */
 export function createComponent<T extends string>(tag: T): FCBetter<T>
-export function createComponent<P extends {}>(tag: FC<P>): FCBetter<P>
+export function createComponent<P extends {}>(tag: FC<P>): FCBetter<FC<P>>
 export function createComponent(tag: any): FCBetter<any> {
   return function (props) {
     const _props = convertChildrenProps(props)
